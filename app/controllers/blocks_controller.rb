1b912112bb9b2b5c920b432baab794788b37ed19
tvcou <tvcou.zyx77@gmail.com>
2015-12-06 08:48:09 +0000

    "make create block vi app/controllers/blocks_controller.rb"

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -12,4 +12,15 @@ class BlocksController < ApplicationController
     @block = Block.new
   end
 
+  def create
+    @block = Block.new(block_params)
+    @block.save
+    redirect_to blocks_path
+  end
+ 
+  private
+    def block_params
+      params[:block].permit(:name, :memo)
+    end
+
 end
