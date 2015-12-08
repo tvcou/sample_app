72c3f87d2e745c16627302b87a8f4f67a54e0996
tvcou <tvcou.zyx77@gmail.com>
2015-12-08 21:16:40 +0000

    "gp mod err messages vi app/models/block.rb"

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,6 +1,8 @@
 class Block < ActiveRecord::Base
 
-  validates :name, presence: true
+  validates :name,
+    presence: { message: "入力してください" },
+    length: { minimum: 3, message: "短すぎ！" }
   validates :memo, presence: true
 
 end
